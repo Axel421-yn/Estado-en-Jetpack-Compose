a1827e7191a9eee43f3b744e0c845ac2bbacc72d
Axel421-yn <s23120222@alumnos.itsur.edu.mx>
2026-09-25 00:34:47 -0600

    chore: crear proyecto Android con Kotlin y Jetpack Compose

---
+++ app/src/main/java/com/codelabs/basicstatecodelab/WellnessScreen.kt
@@ -0,0 +1,4 @@
+package com.codelabs.basicstatecodelab
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+@Composable fun WellnessScreen() { Text("Bienestar diario") }
